48101c6b0e6ebd2c5e8de17299d3f2c7f2580245
Laura <larmartinez2@gmail.com>
2018-09-17 16:46:15 -0700

    show method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,18 @@
 TASKS = [
-  {name: "take out garbage", status: "completed"},
-  {name: "clean room", status: "completed"}
+  { id: 1, name: "take out garbage", completion_date: "completed", description: "every day"},
+  { id: 2, name: "clean room", completion_date: "completed", description: "every week"}
 ]
 
 class TasksController < ApplicationController
   def index
     @tasks = TASKS
+  end
+
+  def show
+    id = params[:id]
+    id = id.to_i
+    @task = TASKS.find do |task|
+      task[:id] == id
+    end
   end
 end

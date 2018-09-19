680fe7b34e4ba27b5856a8fbc5b2ef7934aaf235
Laura <larmartinez2@gmail.com>
2018-09-19 16:59:47 -0700

    first try at create new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@
 #   { id: 1, name: "take out garbage", completion_date: "completed", description: "every day"},
 #   { id: 2, name: "clean room", completion_date: "completed", description: "every week"}
 # ]
-
+require 'pry'
 class TasksController < ApplicationController
   def index
     @tasks = Task.all.order(:completion_date)
@@ -16,5 +16,30 @@ class TasksController < ApplicationController
       #head :not_found
       render :notfound, status: :not_found
     end
+  end
+
+  def create
+    @task = Tasks.new(name: params[:task][:name], completion_date: params[:task][:completion_date]) #instantiate a new task
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to root_path # go to the index so we can see the task in the list
+    else # save failed :(
+      render :new # show the new task form view again, and all past entries create an id with nill
+    end
+  end
+
+  def new
+    @task = Task.new
+    @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id].to_i)
+  end
+
+  def update
+    @task = Task.find(params[:id].to_i)
+  end
+
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show'
+  root "tasks#index"
+
+  get '/tasks', to: 'tasks#index', as: 'tasks'
+  # order matters new must be before id
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+  #
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  #
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  #
+  patch '/tasks/:id', to: 'tasks#update'
+  put '/tasks/:id', to: 'tasks#update'
+  #
+  # delete '/tasks/:id', to: 'tasks#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

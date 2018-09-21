5a7a1a949e5cc3667a3476e07601dad294efed80
Laura <larmartinez2@gmail.com>
2018-09-21 15:21:27 -0700

    migration to change completion date data type from string to date

---
+++ db/migrate/20180921220737_change_date_type_from_string_to_date.rb
@@ -0,0 +1,5 @@
+class ChangeDateTypeFromStringToDate < ActiveRecord::Migration[5.2]
+  def change
+    change_column :tasks, :completion_date, :date, using:"completion_date::date"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_18_211518) do
+ActiveRecord::Schema.define(version: 2018_09_21_220737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
-    t.string "completion_date"
+    t.date "completion_date"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

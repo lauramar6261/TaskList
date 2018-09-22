e2e438997b5258b4a42b536394fc1cde31e90467
Laura <larmartinez2@gmail.com>
2018-09-22 12:35:15 -0700

    auto generate standard routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,35 @@
 Rails.application.routes.draw do
-  root "tasks#index" # this route is called root_path
-  get '/tasks/all', to: 'tasks#all', as: 'all'
-
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-  # order matters new must be before id because :id will match any string, it is just a place holder
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  # the following route is used by the form? yes! it chooses this one if object doesn't have an idea to create a new object = post
-  post '/tasks', to: 'tasks#create'
-  #
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  # the following route is used by the form if the object already exist and to check for this if it it has an id
-  patch '/tasks/:id', to: 'tasks#update' # default http verb for the form
-
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-
-  # when and by who is this path called?
-  # use put to replace entire task, the form will use this path, but you have to tell it to use (action: :put) this http verb because the default for form is patch
-  put '/tasks/:id', to: 'tasks#update'
-
-  #why does this need an alias? if not it will match to edit
+  get '/tasks/all', to: 'tasks#all', as: 'all' # it's getting confused with show
+  resources :tasks
+  root "tasks#index"
   delete '/tasks/:id', to: 'tasks#destroy', as: 'destroy'
-
   get '/task/:id', to: 'tasks#complete', as: 'complete'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# Rails.application.routes.draw do
+#   root "tasks#index" # this route is called root_path
+#   get '/tasks/all', to: 'tasks#all', as: 'all'
+#
+#   get '/tasks', to: 'tasks#index', as: 'tasks'
+#   # order matters new must be before id because :id will match any string, it is just a place holder
+#   get '/tasks/new', to: 'tasks#new', as: 'new_task'
+#   # the following route is used by the form? yes! it chooses this one if object doesn't have an idea to create a new object = post
+#   post '/tasks', to: 'tasks#create'
+#   #
+#   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+#   # the following route is used by the form if the object already exist and to check for this if it it has an id
+#   patch '/tasks/:id', to: 'tasks#update' # default http verb for the form
+#
+#   get '/tasks/:id', to: 'tasks#show', as: 'task'
+#
+#   # when and by who is this path called?
+#   # use put to replace entire task, the form will use this path, but you have to tell it to use (action: :put) this http verb because the default for form is patch
+#   put '/tasks/:id', to: 'tasks#update'
+#
+#   #- why does this need an alias? if not it will match to edit
+#   delete '/tasks/:id', to: 'tasks#destroy', as: 'destroy'
+#
+#   get '/task/:id', to: 'tasks#complete', as: 'complete'
+#
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
